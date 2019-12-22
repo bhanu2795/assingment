3774ca20791cd26f6092d51ed2de1305c77d4900
bhanu2795 <bhanu2795@gmail.com>
2019-12-22 14:17:12 +0530

    image animated from its exact position

--- src/components/animation/index.jsx
+++ src/components/animation/index.jsx
@@ -13,22 +13,22 @@ class Animation extends Component {
         this.state = {};
     }
 
-    _zoomIn = (image) => {
-        const modal = document.getElementById('myModal');
-        const modalImg = document.getElementById('img01');
+    _zoomIn = (e) => {
+        const element = e.target.parentElement.nextSibling;
+        console.log(element.getBoundingClientRect())
+        element.style.visibility = 'visible';
+        setTimeout(() => {
+            element.style.transform = `translate(${(window.innerWidth / 2) - element.getBoundingClientRect().left}px, ${(window.innerHeight / 2) - element.getBoundingClientRect().top}px)`;
+        }, 1000);
 
-        modal.style.display = 'block';
-        modalImg.src = image;
     }
 
-    _zoomOut = () => {
-        const modal = document.getElementById('myModal');
-        const modalImg = document.getElementById('img01');
-        modalImg.className += ' ' + style.out;
+    _zoomOut = (e) => {
+        const element = e.target.parentElement;
         setTimeout(() => {
-            modal.style.display = 'none';
-            modalImg.className = style.modalContent;
-        }, 400);
+            element.style.visibility = 'hidden';
+        }, 1000);
+        element.style.transform = `translate(${(window.innerWidth / 2) - element.getBoundingClientRect().left}px, ${(window.innerHeight / 2) - element.getBoundingClientRect().top}px)`;
     }
 
     render = () => (
@@ -40,36 +40,30 @@ class Animation extends Component {
                     </div>
                     <h3 className={style.headStyle}>Welcome To Animation Component</h3>
                     <div className={style.bexchange_main_form}>
-                        <div className={style.item}>
-                            <img
-                                style={{
-                                    width: 'auto',
-                                    height: '100%',
-                                    cursor: 'pointer',
-                                    marginLeft: '-50px',
-                                }} src={dev1} alt="dev1" onClick={() => this._zoomIn(dev1)} />
+                        <div className={style.wrapper}>
+                            <div className={style.item}>
+                                <img className={style.animationImage} src={dev1} alt="dev1" onClick={this._zoomIn.bind(this)} />
+                            </div>
+                            <div className={style.test} style={{ visibility: 'hidden' }}>
+                                <img className={style.animationImage} src={dev1} alt="dev1" onClick={this._zoomOut.bind(this)} />
+                            </div>
                         </div>
-                        <div className={style.item}>
-                            <img
-                                style={{
-                                    width: 'auto',
-                                    height: '100%',
-                                    cursor: 'pointer',
-                                    marginLeft: '-50px',
-                                }} src={dev2} alt="dev2" onClick={() => this._zoomIn(dev2)} />
+                        <div className={style.wrapper}>
+                            <div className={style.item}>
+                                <img className={style.animationImage} src={dev2} alt="dev2" onClick={this._zoomIn.bind(this)} />
+                            </div>
+                            <div className={style.test} style={{ visibility: 'hidden' }}>
+                                <img className={style.animationImage} src={dev2} alt="dev2" onClick={this._zoomOut.bind(this)} />
+                            </div>
                         </div>
-                        <div className={style.item}>
-                            <img
-                                style={{
-                                    width: 'auto',
-                                    height: '100%',
-                                    cursor: 'pointer',
-                                    marginLeft: '-50px',
-                                }} src={dev3} alt="dev3" onClick={() => this._zoomIn(dev3)} />
+                        <div className={style.wrapper}>
+                            <div className={style.item}>
+                                <img className={style.animationImage} src={dev3} alt="dev3" onClick={this._zoomIn.bind(this)} />
+                            </div>
+                            <div className={style.test} style={{ visibility: 'hidden' }}>
+                                <img className={style.animationImage} src={dev3} alt="dev3" onClick={this._zoomOut.bind(this)} />
+                            </div>
                         </div>
-                    </div>
-                    <div id="myModal" className={style.modal}>
-                        <img className={style.modalContent} id="img01" onClick={() => this._zoomOut()} />
                     </div>
                 </div>
             </div>
